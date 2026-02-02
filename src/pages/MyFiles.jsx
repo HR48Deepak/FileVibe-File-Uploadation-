361aea66da3f518bda1e0f760b1f630b72b9d244
HR48Deepak <deepakpilaniya125@gmail.com>
2026-02-02 12:21:15 +0530

    change

--- src/pages/MyFiles.jsx
+++ src/pages/MyFiles.jsx
@@ -285,8 +285,8 @@ const MyFiles = () => {
                             <p className="text-sm text-gray-500 mb-4">Sharing: <span className="text-slate-800 font-semibold">{shareModal.file?.filename}</span></p>
                             <input type="email" placeholder="Recipient email" className="w-full p-3 border border-gray-300 rounded-lg outline-none mb-6" value={shareModal.email} onChange={(e) => setShareModal({ ...shareModal, email: e.target.value })} />
                             <div className="flex justify-end gap-3">
-                                <button onClick={() => setShareModal({ isOpen: false, file: null, email: '' })} className="px-4 py-2 text-gray-600">Cancel</button>
-                                <button onClick={confirmShare} disabled={loading} className="px-6 py-2 bg-blue-600 text-white rounded-lg"> {loading ? "Sharing..." : "Share"} </button>
+                                <button onClick={() => setShareModal({ isOpen: false, file: null, email: '' })} className="px-4 py-2 text-gray-600 border-2 rounded-lg cursor-pointer">Cancel</button>
+                                <button onClick={confirmShare} disabled={loading} className="px-6 py-2 bg-blue-600 text-white rounded-lg cursor-pointer"> {loading ? "Sharing..." : "Share"} </button>
                             </div>
                         </div>
                     </div>
@@ -299,8 +299,8 @@ const MyFiles = () => {
                             <h3 className="text-xl font-bold mb-2">Are you sure?</h3>
                             <p className="text-gray-500 mb-6">This will permanently remove the file.</p>
                             <div className="flex gap-3">
-                                <button onClick={() => setDeleteModal({ isOpen: false, id: null, publicId: null })} className="flex-1 px-4 py-2 border rounded-lg">Cancel</button>
-                                <button onClick={confirmDelete} className="flex-1 px-4 py-2 bg-red-600 text-white rounded-lg font-semibold">Delete</button>
+                                <button onClick={() => setDeleteModal({ isOpen: false, id: null, publicId: null })} className="flex-1 px-4 py-2 border-2 rounded-lg cursor-pointer">Cancel</button>
+                                <button onClick={confirmDelete} className="flex-1 px-4 py-2 bg-red-600 text-white rounded-lg font-semibold cursor-pointer">Delete</button>
                             </div>
                         </div>
                     </div>
